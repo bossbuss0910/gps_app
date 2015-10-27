6caa77da9da04f209db333dcff13d6810037c4bc
bossbuss0910 <Boss@Piccolo-MacBookAir.local>
2015-10-27 19:27:32 +0900

    first

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , mongoose = require('mongoose');
 
 var app = module.exports = express.createServer();
 
@@ -35,17 +36,57 @@ app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
+// DB
+
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+	u_id: Number, 
+	name: String,
+	g_id: Number,
+	lat:  Number,
+	long: Number
+});
+mongoose.model('User',UserSchema);
+mongoose.connect('mongodb://localhost/gps_app');
+var User = mongoose.model('User');
 
 //socket
-
 var io = require('socket.io').listen(app);
 io.sockets.on('connection', function (socket) {
+	socket.on('msg update',function(){
+		User.find(function(err,docs){
+			socket.emit('msg open',docs);
+			});
+	});
+
+	console.log('connected');
+
 	socket.on('msg send', function (msg) {
 		socket.emit('msg push', msg);
 		socket.broadcast.emit('msg push', msg);
+		//登録
+		var user =new User();
+		user.u_id = msg.u_id;
+		user.name = msg.name;
+		user.g_id = msg.g_id;
+		user.lat = msg.lat;
+		user.long = msg.long;
+		user.save(function(err){
+			if (err){console.log(err)}
+			});
 	});
+	//DBにあるメッセージを削除
+	socket.on('deleteDB', function(drop_user){
+		User.remove({u_id: drop_user.id})
+		User.remove({ u_id: drop_user.id }, function(err, result){
+			    if (err) {
+				    res.send({'error': 'An error has occurred - ' + err});
+				    } else {
+					    console.log('Success: ' + result + ' document(s) deleted');
+					    }
+			    });
+		});
+	  
 	socket.on('disconnect', function() {
-		console.log('disconnected');
-	});
+		console.log('disconnected');});
 });
-
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,23 +1,47 @@
+//クライアント
 $(function() {
 
-var socket = io.connect('http://localhost');
-	socket.on('connect', function() { // 2
-		console.log('connected');
+	var socket = io.connect('http://localhost');
+	
+	socket.on('connect', function() { 
+		console.log('successfully sent');
+		socket.emit('msg update');
 	});
+
 	
 	$('#btn').click(function() {
-		var message = $('#message');
-		console.log(message);
-		socket.emit('msg send', message.val());
+		var u_id = $('#u_id');
+		var name = $('#name');	 
+		var g_id = $('#g_id');
+		var lat = $('#lat');
+		var long = $('#long');
+		socket.emit('msg send', {u_id:u_id.val(),name:name.val(),g_id:g_id.val(),lat:lat.val(),long:long.val()});
 	});
+
+	$('#delete').click(function(){
+		var u_id = $('#u_id');
+		var name = $('#name');
+		socket.emit('deleteDB',{id:u_id.val(),name:name.val()});
+		})
 
 	socket.on('msg push', function (msg) {
 		console.log(msg);
-		var date = new Date();
-		$('#list').prepend($('<dt>' + date + '</dt><dd>' + msg + '</dd>'));
+		$('#list').prepend($('<dt>' + msg.name + '</dt><dd>' + msg.lat + '</dd>'+'<dd>'+msg.long+'</dd>'));
 	});
 	
-	socket.on('msg updateDB', function(msg){
-		console.log(msg);
+	socket.on('msg open', function(msg){
+		//DBが空っぽだったら
+		if(msg.length == 0){
+			return;
+			} else {
+				$('#list').empty();
+				$.each(msg, function(key, value){
+					$('#list').prepend($('<dt>' + value.name + '</dt><dd>' + value.lat + '</dd>'+'<dd>'+value.long+'</dd>'));
+					});   
+				}
+			});
+
+	socket.on('db drop', function(msg){
+//		 $('#list').empty();
 	});
 });
