179e824938df0137abfa6beae79275481645ef05
bossbuss0910 <Boss@Piccolo-MacBookAir.local>
2015-10-31 14:29:13 +0900

    heroku initial

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.configure('production', function(){
 app.get('/gps/:g_name', routes.index);
 app.get('/login', routes.login);
 
-app.listen(3000, function(){
+app.listen(5000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
