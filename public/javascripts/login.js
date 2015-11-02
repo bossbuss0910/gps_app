60053c140c7df2038d6288675dd1aed2baddc557
bossbuss0910 <Boss@Piccolo-MacBookAir.local>
2015-11-02 23:57:43 +0900

    戻す

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,5 +1,5 @@
 //ログイン画面
-var socket = io.connect("https://gps-app-meet.herokuapp.com/");
+var socket = io.connect();
 $(function() {
 	socket.on('connect', function() {
 		console.log('login connect');
